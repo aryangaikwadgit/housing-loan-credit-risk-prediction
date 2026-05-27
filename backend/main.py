9df4fc166ab44f734d372a8febb9c0ab18a828c2
Aryan Gaikwad <aryangaikwad11.work@gmail.com>
2026-05-27 16:33:14 +0530

    linting

--- backend/main.py
+++ backend/main.py
@@ -5,21 +5,18 @@ import pandas as pd
 import joblib
 import sys
 
-
-sys.path.append(
-    r"C:\Main_Drive\work\workarea\projects\credit_risk_prediction\notebook"
-)
+sys.path.append(r"C:\Main_Drive\work\workarea\projects\credit_risk_prediction\notebook")
 
 import feature_engineering
-
 
 # Create FastAPI app
 app = FastAPI()
 
 
-
 # Load trained model
-model = joblib.load(r"C:\Main_Drive\work\workarea\projects\credit_risk_prediction\models\model_rf.pkl")
+model = joblib.load(
+    r"C:\Main_Drive\work\workarea\projects\credit_risk_prediction\models\model_rf.pkl"
+)
 
 # Input schema
 from pydantic import BaseModel
@@ -27,6 +24,7 @@ from pydantic import BaseModel
 
 # Input Schema
 class CustomerData(BaseModel):
+    """Data Class"""
 
     NAME_CONTRACT_TYPE: str
     CODE_GENDER: str
@@ -55,29 +53,21 @@ class CustomerData(BaseModel):
     ORGANIZATION_TYPE: str
 
 
-
 # Home Route
 @app.get("/")
 def home():
-    return {
-        "message":
-        "Home Credit Risk API Running"
-    }
+    return {"message": "Home Credit Risk API Running"}
 
 
 # Prediction Route
 @app.post("/predict")
-def prediction(data:CustomerData):
+def prediction(data: CustomerData):
     input_dict = data.dict()
     input_df = pd.DataFrame([input_dict])
 
     # Prediction
     prediction = model.predict(input_df)[0]
 
-    probability = (model.predict_proba(input_df)[0])
+    probability = model.predict_proba(input_df)[0]
 
-    return {
-        "risk_score": round(probability[1] * 100, 2)
-    }
-
-
+    return {"risk_score": round(probability[1] * 100, 2)}
